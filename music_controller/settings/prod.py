3a57bcc6a780e02b6034ae89e7ad32f3dfe69413
Abbas Anandwala <abbasa5251@hotmail.com>
2022-08-16 03:24:23 +0530

    bug fix

--- music_controller/settings/prod.py
+++ music_controller/settings/prod.py
@@ -23,7 +23,7 @@ DATABASES = {
     }
 }
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
